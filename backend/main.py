782b72afff88d626972ba95ee1e7c793d115562f
jeetu yadav <jeetuyadav2091@gmail.com>
2026-06-22 00:02:51 +0530

    Fix CORS and API URL

--- backend/main.py
+++ backend/main.py
@@ -113,7 +113,11 @@ async def val_err(req, exc):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://localhost:5173",
+        "https://golden-zabaione-c4b334.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
